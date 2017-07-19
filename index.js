0f38aabce93985e8f3fe2853e05f7aaefe117332
hongbang.lhb <hongbang.lhb@alibaba-inc.com>
2017-07-19 19:47:52 +0800

    fix:定义路径bugfix

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function mergeFile(moduleCmdCode, options){
   var rax = fs.readFileSync(__dirname + '/lib/rax.js', 'utf-8');
   var indexBundle = fs.readFileSync(__dirname + '/lib/index.bundle.js', 'utf-8'); //来自辟蹊个人gitlab工程rax-debuger中的代码
   var basePath = options && options.projectPath ? options.projectPath : process.cwd();
-  var mockDataPath = path.join(options.projectPath, '/data/weex-mock.json');
+  var mockDataPath = path.join(basePath, '/data/weex-mock.json');
   var moduleData = fs.readFileSync(mockDataPath, 'utf-8');
 
   var gdc = '{"title": "Rax && WeAPP","pageId": 75527005,"userId": 2202220535,"uuidRqt": "f256eb6b-38ab-412e-acb2-b1670e2b7d03","extParam": "{}","loginUserId": "0","shopId": 112772958,"pathInfo": "shop/activity","path": "shop/activity","pageName": "Page_WeApp","atp_isdpp": "4v112772958","at_isb": "0","at_alis": "1_2202220535"}';
